e4b081dcafbf2cbc06a8fffac16fb9957015b693
Tim van der Zee <timvanderzee@192.168.1.22>
2019-11-05 22:21:24 +0100

    Eerste versie

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="indexStyle.css">
+        <link href="https://fonts.googleapis.com/css?family=Rubik:400,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Reenie+Beanie&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <aside id="info-block">
+        <section class="file-marker">
+                <div>
+                    <div class="box-title">
+                        &nbsp;&nbsp;THE LONELY PROJECT&nbsp;&nbsp;
+                    </div>
+                    <div class="box-contents">
+                        <form id="quote">
+                            <textarea form="quote" type="text" name="quote" cols="40" rows="5">"Vul hier jouw verhaal in over eenzaamheid."</textarea><br>
+                        </form>
+                    </div>
+                </div>
+        </section>
+        </aside>
+
+    </body>
+</html>
